d1fcdc29ce45e2e944468c2f91de66ba1cdae42d
hasan <hasanarrosyidi1@gmail.com>
2024-06-16 05:13:20 +0700

    adjustment: remove unused import

--- app/src/main/java/com/haa/diagnosabullying/ui/screen/result/ResultViewModel.kt
+++ app/src/main/java/com/haa/diagnosabullying/ui/screen/result/ResultViewModel.kt
@@ -1,14 +1,8 @@
 package com.haa.diagnosabullying.ui.screen.result
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.haa.diagnosabullying.data.repository.QuestionRepository
-import com.haa.diagnosabullying.data.resource.local.room.entity.QuestionEntity
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
